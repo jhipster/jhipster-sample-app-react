3fed4a05d63c8d87031b70de9bf7ea9e2b2b2ea6
Pascal Grimaud <pascalgrimaud@gmail.com>
2020-07-04 23:23:04 +0200

    automatic project update

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const { BaseHrefWebpackPlugin } = require('base-href-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
@@ -121,10 +120,10 @@ module.exports = options => ({
     ]),
     new HtmlWebpackPlugin({
       template: './src/main/webapp/index.html',
-      chunksSortMode: 'dependency',
-      inject: 'body'
+      chunksSortMode: 'auto',
+      inject: 'body',
+      base: '/',
     }),
-    new BaseHrefWebpackPlugin({ baseHref: '/' }),
     new MergeJsonWebpackPlugin({
       output: {
         groupBy: [
