94afaec9f1370db04546f23fa52c21c87eb4e67c
Matt Raible <matt@raibledesigns.com>
2024-10-28 22:18:19 -0600

    automatic project update for v8.7.2

--- src/test/java/io/github/jhipster/sample/config/MysqlTestContainer.java
+++ src/test/java/io/github/jhipster/sample/config/MysqlTestContainer.java
@@ -23,7 +23,7 @@ public class MysqlTestContainer implements SqlTestContainer {
     @Override
     public void afterPropertiesSet() {
         if (null == mysqlContainer) {
-            mysqlContainer = new MySQLContainer<>("mysql:9.0.1")
+            mysqlContainer = new MySQLContainer<>("mysql:9.1.0")
                 .withDatabaseName("jhipsterSampleApplicationReact")
                 .withTmpFs(Collections.singletonMap("/testtmpfs", "rw"))
                 .withLogConsumer(new Slf4jLogConsumer(LOG))
